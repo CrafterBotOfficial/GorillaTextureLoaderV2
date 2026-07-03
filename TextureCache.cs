b8c2c2d7429816b4ab421ad3d72868c9775ac7a5
Crafterbot <crafterbot@proton.me>
2026-07-03 16:01:58 -0400

    Do not copy texture on cache game texture & remove inefficient linq

--- TextureCache.cs
+++ TextureCache.cs
@@ -12,14 +12,25 @@ public class TextureCache(bool EnableGameCaching)
 
     public MeshRenderer[] FindRenderersByTextureName(string name, string textureKey)
     {
-        if (cachedMeshRenderers.TryGetValue(name, out var renderers))
-            return renderers;
+        if (cachedMeshRenderers.TryGetValue(name, out var cachedRenderers))
+            return cachedRenderers;
 
-        renderers = [..GameObject.FindObjectsByType<MeshRenderer>(sortMode: FindObjectsSortMode.None)
-            .Where(x => x.sharedMaterial is not null && x.sharedMaterial.HasTexture(textureKey))
-            .Where(x => x.sharedMaterial.GetTexture(textureKey)?.name == name)];
-        cachedMeshRenderers.Add(name, renderers);
-        return renderers;
+        var result = new List<MeshRenderer>();
+        var propertyId = Shader.PropertyToID(textureKey);
+        foreach (var meshRenderer in GameObject.FindObjectsByType<MeshRenderer>(sortMode: FindObjectsSortMode.None)) 
+        {
+            if (meshRenderer.sharedMaterial is null || !meshRenderer.sharedMaterial.HasProperty(propertyId)) continue;
+
+            var texture = meshRenderer.sharedMaterial.GetTexture(propertyId);
+            if (texture is not null && texture.name != name) continue;
+
+            result.Add(meshRenderer);
+        }
+
+        var array = result.ToArray();
+        cachedMeshRenderers.Add(name, array);
+
+        return array;
     }
 
     public Material[] FindMaterialByTextureName(string name, string textureKey = Paths.MAIN_ATLAS_KEY)
@@ -37,41 +48,15 @@ public class TextureCache(bool EnableGameCaching)
     public void CacheGameTextures(string textureName, Texture2D texture)
     {
         if (!EnableGameCaching || cachedGameTextures.ContainsKey(textureName) || texture is null)
-        {
-            // Main.Log($"Not allowed. {textureName} {texture}", BepInEx.Logging.LogLevel.Warning);
             return;
-        }
-
-        Main.Log("Saving default textures for single");
-        var cache = new Texture2D(texture.width, texture.height, texture.format, 0, false)
-        {
-            filterMode = texture.filterMode,
-            anisoLevel = texture.anisoLevel,
-            wrapMode = texture.wrapMode,
-        };
-
-        Graphics.CopyTexture(texture, cache);
-        cachedGameTextures.Add(textureName, cache);
+        cachedGameTextures.Add(textureName, texture);
     }
 
     public void CacheGameTextures(string textureName, Texture2DArray atlas)
     {
         if (!EnableGameCaching || cachedGameTextures.ContainsKey(textureName) || atlas is null)
-        {
-            // Main.Log($"Not allowed. {textureName} {atlas}", BepInEx.Logging.LogLevel.Warning);
             return;
-        }
-
-        Main.Log("Saving default textures");
-        var cache = new Texture2DArray(atlas.width, atlas.height, atlas.depth, atlas.format, 0, false)
-        {
-            filterMode = atlas.filterMode,
-            anisoLevel = atlas.anisoLevel,
-            wrapMode = atlas.wrapMode,
-        };
-
-        Graphics.CopyTexture(atlas, cache);
-        cachedGameTextures.Add(textureName, cache);
+        cachedGameTextures.Add(textureName, atlas);
     }
 
     public Dictionary<string, Texture> GetOriginalGameTextures()
